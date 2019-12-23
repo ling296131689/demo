5395af1d7f1e027c0fc3ddf9fb42bbe346a80f9e
ling296131689 <296131689@qq.com>
2019-12-23 19:50:37 +0800

    第一次测试

---
+++ store/src/main/java/cn/tedu/store/mapper/ProductMapper.java
@@ -0,0 +1,30 @@
+package cn.tedu.store.mapper;
+
+import java.util.List;
+
+import cn.tedu.store.entity.Product;
+
+/**
+ * 处理商品数据的持久层接口
+ */
+public interface ProductMapper {
+	
+	/**
+	 * 根据商品id，查询商品详情
+	 * @param id 商品id
+	 * @return 匹配的商品详情，如果没有匹配的数据，则返回null
+	 */
+	Product findById(Integer id);
+	
+	/**
+	 * 获取“新到好货”列表
+	 * @return “新到好货”列表
+	 */
+	List<Product> findNewList();
+	
+}
+
+
+
+
+
